5bf09715cd9acb71e96ba445fab1359cfd7e161c
MoronSlayer <MoronSlayer@outlook.com>
2021-04-22 22:43:27 +0800

    Init Commit

---
+++ eureka-server/src/main/java/org/pyro/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.pyro.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author MoronSlayer
+ * @date 2021/4/22 22:36
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
