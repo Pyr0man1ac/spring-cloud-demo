d2bcf50a61237bbfd251deff01dd13865303e7fe
MoronSlayer <MoronSlayer@outlook.com>
2021-04-23 20:40:18 +0800

    feat: 启用Feign

--- eureka-client/alpha-service/src/main/java/org/pyro/alpha/service/AlphaServiceApplication.java
+++ eureka-client/alpha-service/src/main/java/org/pyro/alpha/service/AlphaServiceApplication.java
@@ -2,11 +2,13 @@ package org.pyro.alpha.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author MoronSlayer
  * @date 2021/4/23 9:32
  */
+@EnableFeignClients
 @SpringBootApplication
 public class AlphaServiceApplication {
 
--- eureka-client/beta-service/src/main/java/org/pyro/beta/service/BetaServiceApplication.java
+++ eureka-client/beta-service/src/main/java/org/pyro/beta/service/BetaServiceApplication.java
@@ -2,7 +2,13 @@ package org.pyro.beta.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author MoronSlayer
+ * @date 2021/4/23 20:39
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class BetaServiceApplication {
     
