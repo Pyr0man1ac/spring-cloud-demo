24301e035a56827d0b8ae3eb72a3c1a356973809
MoronSlayer <MoronSlayer@outlook.com>
2021-04-26 20:58:34 +0800

    fix: 临时解决core模块install问题

---
+++ core/src/main/java/org/pyro/core/FakeApplication.java
@@ -0,0 +1,17 @@
+package org.pyro.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MoronSlayer
+ * @date 2021/4/26 20:06
+ */
+@SpringBootApplication
+public class FakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeApplication.class, args);
+    }
+
+}
