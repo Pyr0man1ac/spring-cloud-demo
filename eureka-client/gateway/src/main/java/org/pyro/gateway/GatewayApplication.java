2f88e412cf6261cb5be0a32ef6ed6e0125080d3d
MoronSlayer <MoronSlayer@outlook.com>
2021-04-30 23:19:11 +0800

    feat: 服务网关 Zuul

---
+++ eureka-client/gateway/src/main/java/org/pyro/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package org.pyro.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author ZYC
+ * @date 2021/4/29 19:51
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
